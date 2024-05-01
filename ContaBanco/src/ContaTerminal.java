9fb8786ce778b498d033c30a04c5a16fe5e10b43
Felipe Taveira <ftaveira611@gmail.com>
2024-05-01 19:17:25 -0300

    conta banco

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.Locale;
+public class ContaTerminal {
+    public static void main(String[] args){
+        //Criando os scanners
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o seu nome: ");
+        String nomeCliente = scanner.next ();
+        System.out.println("por favor, digite seu sobrenome: ");
+        String sobrenomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite o número da sua conta: ");
+        int numeroConta = scanner.nextInt ();
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        String numeroAgencia = scanner.next ();
+
+        System.out.println("Por favor, digite o seu saldo: ");
+        double saldoConta = scanner.nextDouble();
+
+        //Imprimindo os dados obtidos pelo usuario
+        System.out.println("Olá, " + nomeCliente + " " + sobrenomeCliente + " obrigado(a) por criar uma conta no nosso banco!");
+        System.out.println("Sua agência é: " + numeroAgencia + " Conta: " + numeroConta + " e seu saldo: " + saldoConta + " já está disponível para saque.");
+        scanner.close();
+    }
+}
+
